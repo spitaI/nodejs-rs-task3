a8de6b624f9fb596e82feebf8f3263e5a24c36ad
spitaI <alex.asm.1997@gmail.com>
2021-05-19 01:37:55 +0300

    fix: fix compilation error 1

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
 const Sequelize = require('sequelize');
-                                //database username   password
+//database username   password
 const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+  host: 'localhost',
+  dialect: 'postgres',
+});
 
 sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
+  function success() {
+    console.log('Connected to DB');
+  },
 
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+  function fail(err) {
+    console.log(`Error: ${err}`);
+  }
+);
+
+// Fixed COMPILATION ERROR: sequelize were not exported from module
+module.exports = sequelize;
